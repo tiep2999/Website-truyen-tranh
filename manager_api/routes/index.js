ba21c6fbad1636c9a1f7cdd818bdf0f236a9bcc0
Hong Phuc <avaichuong@gmail.com>
2019-04-09 01:04:07 +0700

    next and previous

--- manager_api/routes/index.js
+++ manager_api/routes/index.js
@@ -1,34 +1,48 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
 var mysql = require('mysql');
 
 const con = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'rootpasswordgiven',
-  database : 'mydb'
+  host: 'localhost',
+  user: 'root',
+  password: 'rootpasswordgiven',
+  database: 'mydb'
 });
 
+var nextAndPre = 0;
+var nextStory = new Array();
 
-// /* GET home page. */
-router.get('/pdf', function (req, res, next) {
-});
+router.get('/read/:id.html', function (req, res, next) {
 
-router.get('/read', function (req, res, next) {
+  var idRead = req.params.id;
+  var idReadAbs = Math.abs(idRead);
 
-  var sql = "select ImgStory from detailStoryChap inner join Story on detailStoryChap.idStory = Story.idStory where Story.idStory = 1";
-  con.query(sql, function (err, results) {
-    var rs = [];
-    for(var i = 0; i<results.length; i++){
-      var str = JSON.stringify(results[i]);
-      str = str.substring(13,str.length-2);
-      rs.push(str);
+  var sql = "select * from detailStoryChap inner join Story on detailStoryChap.idStory = Story.idStory where Story.idStory = ?";
+  con.query(sql, idReadAbs, function (err, results) {
+    if (err) {
+      res.send(err);
     }
-    
-    res.render('./ReadStory/ReadStory', { data: rs });
+    else {
+        nextStory = [...results];
+      res.render('./ReadStory/ReadStory', { data: results[0],len: nextAndPre,max:nextStory.length });
+    }
   });
 });
+
+router.get('/read/next', function (req, res, next) {
+
+  nextAndPre++;
+  res.render('./ReadStory/ReadStory', { data: nextStory[nextAndPre],len: nextAndPre,max:nextStory.length });
+
+});
+
+router.get('/read/previous', function (req, res, next) {
+  nextAndPre--;
+  res.render('./ReadStory/ReadStory', { data: nextStory[nextAndPre],len: nextAndPre,max:nextStory.length });
+
+});
+
+
 
 router.get('/admin', function (req, res, next) {
 
@@ -47,7 +61,7 @@ router.get('/delete/:id', function (req, res, next) {
 
   var idDel = req.params.id;
 
-  con.query("delete from Story where idStory = ?;",idDel, function (err, results) {
+  con.query("delete from Story where idStory = ?;", idDel, function (err, results) {
     res.redirect('/admin');
   });
 
@@ -58,6 +72,9 @@ router.get('/insert', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
+  nextStory = [];
+  nextAndPre = 0;
+  nextStory = 0;
   var sql = "select * from Story";
   con.query(sql, function (err, results) {
     if (err)
@@ -74,9 +91,17 @@ router.get('/search', function (req, res, next) {
 
 });
 
-router.get('/about', function (req, res, next) {
+router.get('/about/:id', function (req, res, next) {
 
-  res.render('./AboutStory/AboutStory');
+  var idAbout = req.params.id;
+
+  var sql = "select * from Story where idStory = ?";
+  con.query(sql, idAbout, function (err, results) {
+    if (err)
+      throw err;
+    else
+      res.render('./AboutStory/AboutStory', { data: results });
+  });
 
 });
 
