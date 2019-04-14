06d54933f178570f75097f8c9f42f96f0a11e249
Hong Phuc <avaichuong@gmail.com>
2019-04-14 22:39:14 +0700

    update logIn-Logout-upload-readStory

--- manager_api/routes/index.js
+++ manager_api/routes/index.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
 var mysql = require('mysql');
 
+//connect dbs
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'rootpasswordgiven',
   database: 'mydb'
 });
+
+//process upload where
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploadFile');
+    // cb(null, './upload2')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+
+var upload = multer({ storage: storage });
+var image = [];
+
+var accessAbility = "awesome";
 
 var nextAndPre = 0;
 var nextStory = new Array();
@@ -23,8 +41,13 @@ router.get('/read/:id.html', function (req, res, next) {
       res.send(err);
     }
     else {
-        nextStory = [...results];
-      res.render('./ReadStory/ReadStory', { data: results[0],len: nextAndPre,max:nextStory.length });
+      nextStory = [...results];
+      res.render('./ReadStory/ReadStory', {
+        data: results[0],
+        len: nextAndPre,
+        max: nextStory.length,
+        accessAbility: accessAbility
+      });
     }
   });
 });
@@ -32,43 +55,85 @@ router.get('/read/:id.html', function (req, res, next) {
 router.get('/read/next', function (req, res, next) {
 
   nextAndPre++;
-  res.render('./ReadStory/ReadStory', { data: nextStory[nextAndPre],len: nextAndPre,max:nextStory.length });
+  res.render('./ReadStory/ReadStory', {
+    data: nextStory[nextAndPre],
+    len: nextAndPre,
+    max: nextStory.length,
+    accessAbility: accessAbility
+  });
 
 });
 
 router.get('/read/previous', function (req, res, next) {
   nextAndPre--;
-  res.render('./ReadStory/ReadStory', { data: nextStory[nextAndPre],len: nextAndPre,max:nextStory.length });
-
-});
-
-
-
-router.get('/admin', function (req, res, next) {
-
-  var sql = "select * from Story";
-  con.query(sql, function (err, results) {
-    if (err)
-      throw err;
-    else
-      res.render('./Admin/AllStory', { data: results })
+  res.render('./ReadStory/ReadStory', {
+    data: nextStory[nextAndPre],
+    len: nextAndPre, max: nextStory.length,
+    accessAbility: accessAbility
   });
 
 });
+
 
 //delete story 
 router.get('/delete/:id', function (req, res, next) {
+  if (accessAbility = "admin") {
+    var idDel = req.params.id;
 
-  var idDel = req.params.id;
-
-  con.query("delete from Story where idStory = ?;", idDel, function (err, results) {
-    res.redirect('/admin');
-  });
+    con.query("delete from Story where idStory = ?;", idDel, function (err, results) {
+      res.redirect('/admin');
+    });
+  }
 
 });
 
 router.get('/insert', function (req, res, next) {
-  res.render('./Admin/insert', { title: 'express nodejs' });
+  res.render('./Admin/insert', { accessAbility: accessAbility });
+});
+
+router.post('/uploadfile', upload.any(), function (req, res, next) {
+
+  var str = req.files[0].path;
+  str = str.split('/');
+  str = '/read/next/'+ str[1];
+  image.push(str);
+  console.log(image);
+  res.status(200).send(req.files);
+});
+
+router.post('/upInfo', function (req, res, next) {
+
+  var Story = {
+    'name': req.body.nameStory,
+    'chap': req.body.chap,
+    'image': image[0],
+    'idStory': -1
+  }
+
+  var sql = "select idStory from Story where nameStory = ?";
+
+  con.query(sql, [Story.name], function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      Story.idStory = result[0].idStory;
+
+      var sql2 = "insert into detailStoryChap (idStory,chap,ImgStory) values (?,?,?);";
+
+      con.query(sql2, [Story.idStory, Story.chap, Story.image], function (err, results) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          res.send("thanh cong");
+        }
+      });
+
+    }
+  })
+
+
 });
 
 router.get('/', function (req, res, next) {
@@ -80,7 +145,10 @@ router.get('/', function (req, res, next) {
     if (err)
       throw err;
     else
-      res.render('./Home/home', { data: results })
+      res.render('./Home/home', {
+        data: results,
+        accessAbility: accessAbility
+      })
   });
 
 });
@@ -100,9 +168,98 @@ router.get('/about/:id', function (req, res, next) {
     if (err)
       throw err;
     else
-      res.render('./AboutStory/AboutStory', { data: results });
+      res.render('./AboutStory/AboutStory', {
+        data: results,
+        accessAbility: accessAbility
+      });
   });
 
 });
+
+
+router.get('/login', function (req, res, next) {
+
+  res.render('./LogIn/Login', {
+    accessAbility: accessAbility
+  });
+
+});
+
+
+//check login
+router.post('/loginform', function (req, res, next) {
+
+  var user = {
+    'Email': req.body.gmail,
+    'Password': req.body.pass
+  };
+  var sql = "select * from userInfo where Email = ? and Password = ?";
+  con.query(sql, [user.Email, user.Password], function (err, results) {
+    console.log(results);
+    if (err) {
+      throw err;
+    }
+    else if (results.length !== 0) {
+      if (results[0].iduserInfo === 1) {
+        var sql = "select * from Story";
+        con.query(sql, function (err, resultsStory) {
+          if (err)
+            throw err;
+          else {
+            setTimeout(() => {
+              accessAbility = "admin";
+              res.render('./Admin/AllStory', {
+                data: resultsStory,
+                accessAbility: accessAbility
+              });
+            }, 0)
+          }
+        });
+      }
+      if (results[0].iduserInfo != 1) {
+        accessAbility = "user";
+        res.redirect('/');
+      }
+    }
+    else {
+      res.send("nhap lai");
+    }
+    console.log(results);
+  });
+
+
+});
+
+router.get('/logout', function (req, res, next) {
+
+  accessAbility = "awesome";
+  res.redirect("/")
+
+});
+
+router.get('/logup', function (req, res, next) {
+
+  res.render('./LogIn/LogUp', {
+    accessAbility: accessAbility
+  });
+
+});
+
+// router.get('/xemsp', function (req, res, next) {
+
+//   var sql = "select * from detailStoryChap";
+//   con.query(sql, function (err, results) {
+//     if (err)
+//       throw err;
+//     else {
+//       res.render('test', {
+//         data: results,
+//         accessAbility: accessAbility
+//       })
+//       console.log(results);
+//     }
+//   });
+
+// });
 
 module.exports = router;
